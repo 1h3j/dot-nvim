3a778b598590104ababd338831ba78ef91ec646b
1h3j <221726394+1h3j@users.noreply.github.com>
2025-09-14 16:14:23 +0800

    Fixed indent blankline taking over the dashboard

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,10 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    exclude = {
+      filetypes = { "dashboard", "alpha", "startify" },
+      buftypes = { "terminal", "nofile" },
+    },
+  },
 }
